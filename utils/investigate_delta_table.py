0c9ceeaf8e2ddd5cf4212bbe230dbe4ab7a7dbf8
k216839 <hoangkimkhanh1907@gmail.com>
2025-08-08 16:58:34 +0700

    update

---
+++ utils/investigate_delta_table.py
@@ -0,0 +1,45 @@
+# Please refer to the following documentation for more information
+# about Delta Lake: https://delta-io.github.io/delta-rs/python/usage.html
+from deltalake import DeltaTable
+from helpers import load_cfg
+from pathlib import Path
+import json
+CFG_FILE = "./config/datalake.yaml"
+
+
+def main():
+    cfg = load_cfg(CFG_FILE)
+    data_cfg = cfg["data"]
+
+    # Load Delta Lake table
+    print("*" * 80)
+    dt = DeltaTable(
+        Path(data_cfg["deltalake_folder_path"])/"olist_customers_dataset",
+        version=0)
+    print("[INFO] Loaded Delta Lake table successfully!")
+
+    # Investigate Delta Lake table
+    print("*" * 80)
+    print("[INFO] Delta Lake table schema:")
+    print(json.loads(dt.schema().to_json()))
+    print("*" * 40)
+    print("[INFO] Delta Lake table's current version:")
+    print(dt.version())
+    print("*" * 40)
+    print("[INFO] Delta Lake files:")
+    print(dt.file_uris())
+    print("*" * 40)
+
+    # Query some data
+    print("[INFO] Querying some data from the Delta Lake table:")
+    print(dt.to_pandas(columns=["customer_id"]))
+    print("*" * 40)
+
+    # Investigate history of actions performed on the table
+    print("[INFO] History of actions performed on the table")
+    print(dt.history())
+    print("*" * 40)
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils/write_delta_table.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+from glob import glob
+
+import pandas as pd
+from helpers import load_cfg
+
+from deltalake.writer import write_deltalake
+
+CFG_PATH = "./config/datalake.yaml"
+
+if __name__ == "__main__":
+    cfg = load_cfg(CFG_PATH)
+
+    csv_files = glob("./data/olist/*.csv")
+
+    # Write data into deltalake format
+    for csv_file in csv_files:
+        file_name = csv_file.split("/")[-1].split(".")[0]
+        df = pd.read_csv(csv_file)
+        if os.path.exists(
+            os.path.join(cfg["data"]["deltalake_folder_path"], file_name)
+        ):
+            shutil.rmtree(
+                os.path.join(cfg["data"]["deltalake_folder_path"], file_name)
+            )
+        write_deltalake(
+            os.path.join(cfg["data"]["deltalake_folder_path"], file_name), df
+        )
+        print(f"Generated the file {file_name} successfully!")
